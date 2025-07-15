76db6378cc34a6729b8af3b6ac263366fbc6590d
Mainul Islam <mainulislam6168@gmail.com>
2025-07-15 18:03:36 +0600

    terraform data types and variables

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -13,3 +13,47 @@ output "x11" {
   # We can call variable with string by using ${}
   value = "Value of x1 is ${var.x1}"
 }
+
+## Data Types of Terraform
+# String --> need to be given in double quotes
+# Number --> no need of quote
+# Boolean --> no need of quote
+
+variable "string1" {
+  default = "Hello World"
+}
+
+variable "number1" {
+  default = 100.50
+}
+variable "boolean1" {
+  # Terraform support only true or false
+  default = true
+}
+
+## Variables types
+# plain
+# list
+# map / dict
+
+variable "plain" {
+  default = "dnf"
+}
+
+variable "list" {
+  default = ["ab", 32, true]
+}
+
+variable "map" {
+  default = {
+    course = "devOps"
+    duration = 3.5
+  }
+}
+# get the value
+output "list_value1" {
+  value = var.list[0]
+}
+output "map_name" {
+  value = var.map["course"]
+}
